0da3651bd0163d9779ff0796b3e239fd5264c17f
kulotsystems <kulotsystems@gmail.com>
2022-04-13 14:29:41 +0800

    Lesson 10: Filters

--- app/app.js
+++ app/app.js
@@ -2,7 +2,27 @@ var myNinjaApp = angular.module('myNinjaApp', []);
 
 myNinjaApp.controller('NinjaController', ['$scope', function($scope) {
 
-    $scope.message = "hey y'all";
+    $scope.ninjas = [
+        {
+            name: 'Yoshi',
+            belt: 'green',
+            rate: 50
+        },
+        {
+            name: 'Crystal',
+            belt: 'yellow',
+            rate: 30
+        },
+        {
+            name: 'Ryu',
+            belt: 'orange',
+            rate: 10
+        },
+        {
+            name: 'Shaun',
+            belt: 'black',
+            rate: 1000
+        }
+    ];
 
-    $scope.ninjas = ['yoshi', 'crystal', 'ryu', 'shaun'];
 }]);
