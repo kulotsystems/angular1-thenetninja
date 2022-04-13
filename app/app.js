d12e2337e11a0a7c03fe3ef096dd9c8efe3a9267
kulotsystems <kulotsystems@gmail.com>
2022-04-13 15:43:42 +0800

    Lesson 12: ng-show directive

--- app/app.js
+++ app/app.js
@@ -6,22 +6,26 @@ myNinjaApp.controller('NinjaController', ['$scope', function($scope) {
         {
             name: 'Yoshi',
             belt: 'green',
-            rate: 50
+            rate: 50,
+            available: true
         },
         {
             name: 'Crystal',
             belt: 'yellow',
-            rate: 30
+            rate: 30,
+            available: false
         },
         {
             name: 'Ryu',
             belt: 'orange',
-            rate: 10
+            rate: 10,
+            available: false
         },
         {
             name: 'Shaun',
             belt: 'black',
-            rate: 1000
+            rate: 1000,
+            available: true
         }
     ];
 
