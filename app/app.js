f53550e3d8b626f5c4ebf84fd5815f41c431a3b3
kulotsystems <kulotsystems@gmail.com>
2022-04-14 12:37:15 +0800

    Lesson 16: Views and Routes

--- app/app.js
+++ app/app.js
@@ -1,4 +1,18 @@
-var myNinjaApp = angular.module('myNinjaApp', []);
+var myNinjaApp = angular.module('myNinjaApp', ['ngRoute']);
+
+myNinjaApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'views/home.html'
+        })
+        .when('/directory', {
+            templateUrl: 'views/directory.html',
+            controller : 'NinjaController'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
+}]);
 
 myNinjaApp.controller('NinjaController', ['$scope', function($scope) {
 
